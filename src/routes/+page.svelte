96bf4f0ce30b08a0e65b73b6188788de1015b843
adam2am <scenereproduct@gmail.com>
2025-04-26 13:29:32 +0500

    poc-svelte-with-civet

---
+++ src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="civet">
+	message := "Hello Civet!"
+</script>
+
+<div class="container">
+	<h1 class="title">Welcome to SvelteKit</h1>
+	<p class="description">
+		Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+	</p>
+	<p class="description">
+		{message}
+	</p>
+</div>
+
+<style>
+	.container {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+		text-align: center;
+	}
+
+	.title {
+		font-size: 3rem;
+		margin-bottom: 1rem;
+	}
+
+	.description {
+		font-size: 1.5rem;
+	}
+</style>
---
+++ svelte.config.js
@@ -0,0 +1,21 @@
+import adapter from '@sveltejs/adapter-auto';
+// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'; // We'll replace this
+import { sveltePreprocess } from 'svelte-preprocess-with-civet';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
+	preprocess: sveltePreprocess({
+		// Defaults should handle lang="civet"
+	}),
+
+	kit: {
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		adapter: adapter()
+	}
+};
+
+export default config;
